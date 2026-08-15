94fab28f0ac5e71712a948f25482f043c5f48161
Ángel Reyes <angeljreyes@proton.me>
2026-08-15 17:48:30 -0400

    feat(scripts): update neovim arch setup script

--- scripts/scripts/archdeps/nvim.sh
+++ scripts/scripts/archdeps/nvim.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-if ! command -v yay &> /dev/null; then
-	echo "You need yay to install these dependencies"
-	exit 1
-fi
-
-yay -S --needed git neovim fd ripgrep xclip ttf-jetbrains-mono-nerd unzip imagemagick nvm wget tree-sitter-cli jq dotnet-sdk\
-	tectonic ghostscript
-source /usr/share/nvm/init-nvm.sh
-nvm install --lts
+sudo pacman -S --needed git neovim fd ripgrep xclip wl-clipboard ttf-jetbrains-mono-nerd unzip imagemagick mise wget\
+	tree-sitter-cli jq dotnet-sdk tectonic ghostscript
+eval "$(mise activate)"
+mise use -g node
 dotnet tool install -g dotnet-outdated-tool
-dotnet tool install -g dotnet-script
